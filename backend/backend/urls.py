f59778a593f3d3f2632be4cf195a82fb2502a027
jarriagadac <jarrcan@gmail.com>
2024-12-16 14:39:11 -0300

    feat(backend): corregir rutas de URL para incluir barras finales y ajustar expresiones regulares en producción

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -27,15 +27,15 @@ from django.views.generic import TemplateView
 from backend.views import LoginView
 
 urlpatterns = [
-    path("/admin", admin.site.urls),
-    path("/api/auth", LoginView.as_view()),
-    path("/api/catalog", include("piezas.urls")),
+    path("admin/", admin.site.urls),
+    path("api/auth/", LoginView.as_view()),
+    path("api/catalog/", include("piezas.urls")),
 ]
 
 if settings.FRONTEND_DEV:
     urlpatterns += [re_path(r"^$", lambda request: redirect(settings.FRONTEND_URL, permanent=False))]
 else:
-    urlpatterns += [re_path(r"^(?!/api/catalog|/admin|/api/auth).*", TemplateView.as_view(template_name="index.html"))]
+    urlpatterns += [re_path(r"^(?!api/catalog/|admin/|api/auth/).*", TemplateView.as_view(template_name="index.html"))]
     urlpatterns += [re_path(r"^manifest.json", TemplateView.as_view(template_name="manifest.json"))]
 
 if settings.DEBUG:
